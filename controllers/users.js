1887cb2f471cd6d1d9ae3896f88a2945d0ba3063
ZverevK <zverev.a.konstantin@yandex.ru>
2020-11-20 16:22:59 +0300

    create users c

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
@@ -20,8 +21,13 @@ module.exports.getUser = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
     .then((user) => res.status(200).send({ user }))
     .catch((err) => res.status(400).send({ message: `Данные не отправлены ${err}` }));
 };
