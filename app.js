310cbe36f0f1d3f303b69190fb2dfb81c284f16d
ZverevK <zverev.a.konstantin@yandex.ru>
2020-11-24 00:21:45 +0300

    create owner validation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cards = require('./routes/cards');
 const users = require('./routes/users');
+const { login, createUser } = require('./routes/users');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -15,15 +16,10 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5fabc315426a985e97df94fc',
-  };
-
-  next();
-});
-app.use('/', cards);
-app.use('/', users);
+app.use('/cards', cards);
+app.use('/users', users);
+app.use('/signup', createUser);
+app.use('/signup', login);
 app.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемые данные не найдены' });
 });
