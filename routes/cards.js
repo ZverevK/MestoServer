32ef446c753950db60d5aa9e05b299cd33a6b52a
ZverevK <zverev.a.konstantin@yandex.ru>
2020-11-23 23:54:04 +0300

    add auth verification

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { getCards, deleteCard, createCard } = require('../controllers/cards');
+const auth = require('../middlewares/auth');
 
-router.get('/cards', getCards);
-router.post('/cards', createCard);
-router.delete('/cards/:id', deleteCard);
+router.get('/cards', auth, getCards);
+router.post('/cards', auth, createCard);
+router.delete('/cards/:id', auth, deleteCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 const {
   getUsers, getUser, createUser, login,
 } = require('../controllers/users');
+const auth = require('../middlewares/auth');
 
-router.get('/users', getUsers);
-router.get('/users/:id', getUser);
+router.get('/users', auth, getUsers);
+router.get('/users/:id', auth, getUser);
 router.post('/signup', createUser);
 router.post('/signin', login);
 
