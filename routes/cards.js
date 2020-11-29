7984d46ed3fe97675044763ceab3f465165d423d
ZverevK <zverev.a.konstantin@yandex.ru>
2020-11-29 16:57:44 +0300

    add Joi validation in router

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getCards, deleteCard, createCard } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required(),
+  }).unknown(true),
+}), createCard);
 router.delete('/:id', deleteCard);
 
 module.exports = router;
