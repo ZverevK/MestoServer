7dae6ee80a8af8194bd78e1be657fbaad71a3ea3
ZverevK <zverev.a.konstantin@yandex.ru>
2020-11-29 17:33:08 +0300

    add crash-test

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
