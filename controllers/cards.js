3209ed1c459738bfda9720aef3ff03808f81d208
ZverevK <zverev.a.konstantin@yandex.ru>
2020-11-29 16:45:09 +0300

    edit controllers

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,32 +1,41 @@
 const Card = require('../models/card');
 
-module.exports.getCards = (req, res) => {
+const ForbiddenError = require('../errors/forbidden-error');
+const NotFoundError = require('../errors/not-found-error');
+const BadRequestError = require('../errors/bad-request-error');
+
+module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((data) => res.status(200).send(data))
-    .catch((err) => res.status(400).send({ message: `Неверный запрос ${err}` }));
+    .then((cards) => res.status(200).send(cards))
+    .catch(next);
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   Card.findByIdAndRemove(req.params.id).orFail(new Error('NotValidId'))
     .then((data) => {
       if (data.owner.toString() !== req.user._id) {
-        return res.status(403).send({ message: 'Можно удалять только свои карточки' });
+        throw new ForbiddenError('Нельзя удалить чужую карточку');
       }
       return res.status(200).send(data);
     })
     .catch((err) => {
       if (err.message === 'NotValidId') {
-        return res.status(404).send({ message: 'Такой карточки нет!' });
+        return next(new NotFoundError(`Карточка не найдена ${req.params.id}`));
       } if (err.name === 'CastError') {
-        return res.status(400).send({ message: `Ошибка валидации id карточки ${req.params.id}` });
+        return next(new BadRequestError(`Неверный запрос ${req.params.id}`));
       }
-      return res.status(500).send({ message: err.message });
+      return next(err);
     });
 };
 
-module.exports.createCard = (req, res) => {
+module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((user) => res.status(200).send({ user }))
-    .catch((err) => res.status(400).send({ message: `Данные не отправлены ${err}` }));
+    // eslint-disable-next-line consistent-return
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(new BadRequestError(`Неверный запрос ${err.message}`));
+      }
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -2,43 +2,42 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
+const AutharizationError = require('../errors/autharization-error');
+const ConflictError = require('../errors/conflict-error');
+const NotFoundError = require('../errors/not-found-error');
+const BadRequestError = require('../errors/bad-request-error');
+
 const { JWT = 'dev-secret' } = process.env;
 
-module.exports.getUsers = (req, res) => {
+module.exports.getUsers = (req, res, next) => {
   User.find({})
     .then((data) => res.status(200).send(data))
-    .catch((err) => res.status(400).send({ message: `Неверный запрос ${err}` }));
+    .catch(next);
 };
 
-module.exports.getUser = (req, res) => {
+module.exports.getUser = (req, res, next) => {
   User.findById(req.params.id)
     .then((data) => {
       if (!data) {
-        return res.status(404).send({ message: 'Пользователь не найден' });
+        return next(new NotFoundError('Пользователь с таким id не найдён'));
       }
       return res.status(200).send(data);
     })
-    .catch((err) => {
-      res.status(400).send({ message: `Неверный запрос ${err}` });
-    });
+    .catch((err) => next(new BadRequestError(`Неверный id ${err.message}`)));
 };
 
 // eslint-disable-next-line consistent-return
-module.exports.createUser = (req, res) => {
-  const pattern = new RegExp(/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/);
+module.exports.createUser = (req, res, next) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
-  if (!pattern.test(password)) {
-    return res.status(400).send({ message: 'Пороль должен содержать заглавные и прописные буквы, цифры и состоять из не менее, чем 8 знаков' });
-  }
   bcrypt.hash(password, 10)
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Проверьте правильность данных' });
+        return next(new NotFoundError('Проверьте правильность данных'));
       }
       return res.status(200).send({
         _id: user._id,
@@ -50,29 +49,25 @@ module.exports.createUser = (req, res) => {
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: `Ошибка валидации ${err.message}` });
+        return next(new BadRequestError(`Ошибка валидации ${err.message}`));
       }
       if (err.name === 'MongoError' && err.code === 11000) {
-        return res.status(409).send({ message: 'Данный email занят' });
+        return next(new ConflictError('Данный email уже используется'));
       }
-      return res.status(500).send({ message: err.message });
+      return next(err);
     });
 };
 
-module.exports.login = (req, res) => {
+module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
+      // создадим токен
       const token = jwt.sign(
         { _id: user._id },
         JWT,
-        { expiresIn: '7d' },
       );
       res.send({ token });
     })
-    .catch((err) => {
-      res
-        .status(401)
-        .send({ message: err.message });
-    });
+    .catch((err) => next(new AutharizationError(`${err.message}`)));
 };
