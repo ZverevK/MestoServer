8b3b9aefba53a9d9c28861916320263951a5c584
ZverevK <zverev.a.konstantin@yandex.ru>
2020-11-29 16:17:58 +0300

    create errors files

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cards = require('./routes/cards');
 const users = require('./routes/users');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser } = require('./controllers/users');
 
 const app = express();
@@ -17,11 +18,16 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
 app.post('/signup', createUser);
 app.post('/signin', login);
 app.use(auth);
 app.use('/cards', cards);
 app.use('/users', users);
+
+app.use(errorLogger);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Данные не найдены' });
